29d132dfb767c46ff049cc9388117a0f10505e87
Florin <florin@test.ro>
2013-11-22 22:31:08 +0100

    introducing properties file

--- src/main/java/com/fbratu/imdb/Main.java
+++ src/main/java/com/fbratu/imdb/Main.java
@@ -1,6 +1,7 @@
 package com.fbratu.imdb;
 
 import java.io.IOException;
+import java.util.Properties;
 import java.util.regex.Pattern;
 
 /**
@@ -8,20 +9,28 @@ import java.util.regex.Pattern;
  */
 public class Main {
 
-    private static final String IMDB_URL_PREFIX = "http://www.imdb.com/title/tt";
+    private static final String IMDB_URL_PREFIX_PROP = "imdb.url.prefix";
 
     private static final String RATING_PATTERN = "<div class=\"titlePageSprite star-box-giga-star\"> (\\d.\\d) </div>";
 
-    public static void main(String args[]) {
-        String url = IMDB_URL_PREFIX + args[0];
-        PageParser parser = new PageParser(Pattern.compile(RATING_PATTERN));
-        try {
-            parser.parse(url);
-            System.out.println("Rating for " + url + " is " + parser.getRating());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private static final String PROPERTIES_FILE = "search.properties";
 
+    public static void main(String args[]) throws IOException {
+           String urlSuffix = args[0];
+           Main main = new Main();
+        main.search(urlSuffix);
+    }
+
+    private void search(String urlSuffix ) throws IOException {
+        // load search properties
+        Properties props = new Properties();
+        props.load(getClass().getResourceAsStream(PROPERTIES_FILE));
+
+        String urlPrefix = props.getProperty(IMDB_URL_PREFIX_PROP);
+        String url = urlPrefix + urlSuffix;
+        PageParser parser = new PageParser(Pattern.compile(RATING_PATTERN));
+        parser.parse(url);
+        System.out.println("Rating for " + url + " is " + parser.getRating());
     }
 
 }
