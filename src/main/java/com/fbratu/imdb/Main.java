c137ce0818412d0e1e64880f4914da49c5fa690f
Florin <florin@test.ro>
2013-11-22 22:45:11 +0100

    add lower and upper bounds to rating. and move rating pattern to props fiile

--- src/main/java/com/fbratu/imdb/Main.java
+++ src/main/java/com/fbratu/imdb/Main.java
@@ -9,29 +9,38 @@ import java.util.regex.Pattern;
  */
 public class Main {
 
+    private static final String PROPERTIES_FILE = "search.properties";
+
     private static final String IMDB_URL_PREFIX_PROP = "imdb.url.prefix";
 
     private static final String RATING_PATTERN_PROP = "rating.pattern";
 
-    private static final String PROPERTIES_FILE = "search.properties";
+    private static final String RATING_MIN_PROP = "rating.min";
+
+    private static final String RATING_MAX_PROP = "rating.max";
 
     public static void main(String args[]) throws IOException {
-           String urlSuffix = args[0];
-           Main main = new Main();
+        String urlSuffix = args[0];
+        Main main = new Main();
         main.search(urlSuffix);
     }
 
-    private void search(String urlSuffix ) throws IOException {
+    private void search(String urlSuffix) throws IOException {
         // load search properties
         Properties props = new Properties();
         props.load(getClass().getResourceAsStream(PROPERTIES_FILE));
 
+        double minRating = Double.parseDouble(props.getProperty(RATING_MIN_PROP));
+        double maxRating = Double.parseDouble(props.getProperty(RATING_MAX_PROP));
         String urlPrefix = props.getProperty(IMDB_URL_PREFIX_PROP);
         String url = urlPrefix + urlSuffix;
         String ratingPattern = props.getProperty(RATING_PATTERN_PROP);
         PageParser parser = new PageParser(Pattern.compile(ratingPattern));
         parser.parse(url);
-        System.out.println("Rating for " + url + " is " + parser.getRating());
+        double rating = parser.getRating();
+        if(minRating < rating && rating < maxRating) {
+            System.out.println(url);
+        }
     }
 
 }
